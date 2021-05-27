32c8cdb5867dec62930341dcac190fbc19a6a6bf
Keshav Kishor Ram <ramabishek40@gmail.com>
2021-05-27 14:37:05 +0545

    Add Search and Trending component

--- src/components/Right.jsx
+++ src/components/Right.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Search from "./sub-components/Search";
+import TrendsForYou from "./sub-components/TrendsForYou";
 export default function Right() {
   return (
     <div className="right">
       <Search />
+      <TrendsForYou />
     </div>
   );
 }
