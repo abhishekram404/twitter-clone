56902f240fc5a18e6c19cc6739ff26191415dd04
Keshav Kishor Ram <ramabishek40@gmail.com>
2021-05-27 18:39:24 +0545

    Add Message Popup component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./styles/App.css";
 import Main from "./components/Main";
+import MessagePopup from "./components/MessagePopup";
 function App() {
   return (
     <div className="App">
       <Main />
+      <MessagePopup />
     </div>
   );
 }
