95dd42810b00b8358177d4035a666842d06a7612
Samarth <subedi42samarth@gmail.com>
2021-05-29 21:48:20 +0545

    Completed the middle component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Left from "./Left";
 import Middle from "./Middle";
 import Right from "./Right";
+
 export default function Main() {
   return (
     <div className="container">
--- src/components/Middle.jsx
+++ src/components/Middle.jsx
@@ -1,12 +1,18 @@
 import React from "react";
-import {Top} from './sub-components/Middle-top';
+import { Top } from "./sub-components/Middle-top";
+import NewTweet from "./sub-components/New-tweet";
+import Post from './sub-components/Post';
 
 export default function Middle() {
   return (
     <div className="middle">
-
       <Top />
-
+      <NewTweet />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
     </div>
   );
 }
